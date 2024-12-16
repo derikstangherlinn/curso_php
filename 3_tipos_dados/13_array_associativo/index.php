22246b32b47dede5d0bd2114d32925de3ee7726f
derikstangherlinn <derikst2@gmail.com>
2024-12-16 14:35:37 -0300

    aray_associativa

---
+++ 3_tipos_dados/13_array_associativo/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    $arr = ['nome' => 'Shark', 'patas' => 4, 'cor' => 'Marrom'];
+
+    echo $arr ['nome']; //Imprimiu no localost o nome 'Shark'
+    echo "<br>";
+    print_r($arr); // Imprimiu no localhost a varival$arr 
+    echo "<br>";
+    echo $arr ['patas']; // Imprimiu '4' no localhost
+    $arrAssoc = ['chave' => 'valor', 'bool' => true]; // true = 1
+    echo "<br>";
+    print_r ($arrAssoc); //Imprimiu no localhost a arrayAssoc.
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+    $arr1 = ['carros', 2];
+    $p = ['nome' => 'DERIK', 'idade' => '20', 'cor' => 'BRANCO'];
+    echo "<br>";
+    print_r ($p);
+    echo "<br>";
--- 3_tipos_dados/15_exercicio_7/index.php
+++ 3_tipos_dados/15_exercicio_7/index.php
@@ -2,7 +2,7 @@
 
     $pessoa = [
     'nome' => 'derik',
-    'idade' => 20,
+    'idade' => 17,
     'peso' => 65,
     'cor' => 'branco'
     ];
@@ -18,7 +18,8 @@
 
     echo "Meu nome é $nome, tenho $idade anos, peso $peso Kg e sou $cor";
     echo "<br>";
-    if ($pessoa ['idade'] >= $maioridade) {
-        echo "Ele é maior de idade";
+    if ($pessoa ['idade'] >= $maioridade) {   //Pega a informação dentro da variavel $pessoa, pega a array idade que = (ex: 17)
+        echo "Ele é maior de idade";            // e pergunta se é maior ou igual a 18 ($maioridade)
+    } else {
+        echo "Ele não é maior de idade";
     }
-
