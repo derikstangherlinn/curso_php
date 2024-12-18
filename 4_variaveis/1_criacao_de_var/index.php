98a6054c5596dfbc05553e126816a45bdc5ac058
derikstangherlinn <derikst2@gmail.com>
2024-12-18 13:11:53 -0300

    att

--- 4_variaveis/1_criacao_de_var/index.php
+++ 4_variaveis/1_criacao_de_var/index.php
@@ -17,3 +17,6 @@
 
     $num2 = number_format(($num * 0.10),2,',','.');
     echo $num2;
+
+    $velocidadeMaxima = 100;    //Forma de visualizar a variavel de uma maneira mais clara.
+    $velocidade_minima = 100;
---
+++ 4_variaveis/3_exercicio_9/index.php
@@ -0,0 +1,6 @@
+<?php
+
+$a = 10; //Int
+$b = 100.10; //Float
+$s = number_format(($a + $b), 2, ',', '.');
+    echo $s;
