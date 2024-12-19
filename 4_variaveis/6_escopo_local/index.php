33a2e9a4bdf4429c9af8656ee8f6d3142eefa2dd
derikstangherlinn <derikst2@gmail.com>
2024-12-19 13:53:14 -0300

    att

---
+++ 4_variaveis/6_escopo_local/index.php
@@ -0,0 +1,36 @@
+<?php
+
+    $x = 10;
+
+    echo "$x global <br>";
+
+    function teste() {
+
+        $x = 5;
+
+        echo "$x local <br>";
+
+    }
+
+    teste();
+
+    echo "$x global <br>";
+    
+    teste();
+
+    function testando(){
+
+        $x = 12;
+
+        echo "$x local 2 <br>";
+
+    }
+
+    $x = 99;
+
+    testando(); // function 12; no localhost
+
+    teste();    // 5; no localhost
+
+    echo "$x global <br>"; // 99; no localhost
+
