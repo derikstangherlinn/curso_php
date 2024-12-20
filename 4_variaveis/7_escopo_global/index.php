9ed374c92f488df3061ad811ca7d3a9a10632482
derikstangherlinn <derikst2@gmail.com>
2024-12-20 13:12:51 -0300

    att

--- 4_variaveis/7_escopo_global/index.php
+++ 4_variaveis/7_escopo_global/index.php
@@ -1,39 +1,35 @@
 <?php
 
     $teste = "asd";
-
-    echo "$teste global 1 <br>";
+    echo "$teste global 1 <br>"; // asd global 1 no localhost
 
     if ( 5 > 2 ) {
-
         $teste = "dsa";
 
-        echo "$teste if <br>";
-
+        echo "$teste if <br>"; // dsa if no localhost
     }
 
-    echo "$teste global 2 <br>";
+    echo "$teste global 2 <br>"; // dsa global 2
 
     function funcao() {
 
-    $teste = "xsxs";
+    global $teste;  
 
     echo "$teste local <br>";
 
     }
 
-    funcao();
+    funcao(); // dsa local 
 
     function testandoGlobal() {
 
         global $teste;
 
-        $teste = 2;
+        $teste = 2; 
 
         echo "$teste global função <br>";
-
     }
 
-    testandoGlobal();
-
-    echo "$teste global 3 <br>";
+    testandoGlobal(); // 2 global funcao no localhost
+    
+    echo "$teste global 3 <br>"; // 2 global 3 no localhost
