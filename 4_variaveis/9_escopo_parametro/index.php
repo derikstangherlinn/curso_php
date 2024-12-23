8edbff4f55df4f696370542551a5d10d4fdf2911
derikstangherlinn <derikst2@gmail.com>
2024-12-23 14:46:19 -0300

    att

--- 4_variaveis/9_escopo_parametro/index.php
+++ 4_variaveis/9_escopo_parametro/index.php
@@ -10,8 +10,4 @@
     soma (2, 4); // Imprimiu a soma dos numeros no localhost.
     soma (6, 8);
     soma (10, 10); 
-
-
-echo "A soma dos números 2 e 4 é: " . soma(2, 4) . "<br>"; // Usa o operador de concatenação '.' para unir a string com o resultado da soma.
-echo "A soma dos números 6 e 8 é: " . soma(6, 8) . "<br>";
-echo "A soma dos números 10 e 10 é: " . soma(10, 10) . "<br>";
+  
