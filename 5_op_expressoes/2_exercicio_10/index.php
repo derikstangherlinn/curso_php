10c2839c231dfd4436215b91a5472555cd77e372
derikstangherlinn <derikst2@gmail.com>
2024-12-26 14:35:28 -0300

    att

---
+++ 5_op_expressoes/2_exercicio_10/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$a = 10;
+$b = 15;
+$c = 50;
+
+$operacao = $a - $b / $c * $a;
+$operacao2 = ($a + $b) / $c * $a; 
+
+echo $operacao;
+echo "<br>";
+echo $operacao2;
+echo "<br>";
---
+++ 5_op_expressoes/4_exercicio_11/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$a = "5";
+$b = 12;
+$expressao = $a * $b;
+
+/*
+Ou
+$operacao = "5" * 12;
+echo $operacao
+*/
+
+echo $expressao . "<br>";
+echo gettype($expressao);
+echo "<br>";
+echo gettype([]); //Array
+echo "<br>";
+echo gettype(12.2); //Float
+echo "<br>";
+echo gettype ("teste"); // String
