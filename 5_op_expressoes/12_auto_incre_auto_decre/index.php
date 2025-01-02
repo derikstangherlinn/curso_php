d0b0bac3fa27de86e1d40b934c54760ca2212d89
derikstangherlinn <derikst2@gmail.com>
2025-01-02 15:00:59 -0300

    att

--- 5_op_expressoes/12_auto_incre_auto_decre/index.php
+++ 5_op_expressoes/12_auto_incre_auto_decre/index.php
@@ -3,8 +3,8 @@
 $x = 0;
 $y = 0;
 
-$x++;
-$y = $y + 1;
+$x++; // Foi como estivesse sendo escrito $x + 1.
+$y = $y + 1; // Outra forma de escrever usando o +1.
 
 echo "x: " . $x . "<br>";
 echo "y: " . $y . "<br>";
@@ -12,8 +12,8 @@ echo "y: " . $y . "<br>";
 $n = 10;
 $m = 10;
 
-$n--;
-$m = $m - 1;
+$n--; // Mesmo caso, tirou 1 de 10 utilizando os sinais --.
+$m = $m - 1; //Tirou 1 de 10 utilizando a subtração.
 
 echo "n: " . $n . "<br>";
 echo "m: " . $m . "<br>";
