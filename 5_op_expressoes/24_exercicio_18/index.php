a57d193da4e43a650b997bcd98ba6d4e2d004f8c
derikstangherlinn <derikst2@gmail.com>
2025-01-07 16:24:04 -0300

    att

--- 5_op_expressoes/24_exercicio_18/index.php
+++ 5_op_expressoes/24_exercicio_18/index.php
@@ -1,21 +1,21 @@
 <?php
 
 // comparacao 1
-if(12 < 5 || "João" === "João") {
+if(12 < 5 || "João" === "João") { // V F = V, pois uma delas foi verdadeira.
     echo "A operacao 1 é verdadeira <br>";
 }
 
 // comparacao 2
-if(1 > 5 || 1) {
+if(1 > 5 || 1) { // V V = V
     echo "A operacao 2 é verdadeira <br>";
 }
 
-//comparacao 3
-if( 20 === "20" && 51 >= 31) {
+//comparacao 3 
+if( 20 === "20" && 51 >= 31) { // F V = F por conta do &&
     echo "A operacao 3 é verdadeira <br>";
 }
 
 //comparacao teste
-if (20 == "20" && 51 >= 31) {
+if (20 == "20" && 51 >= 31) { // V V = V 
     echo "A operacao teste é verdadeira <br>";
 }
