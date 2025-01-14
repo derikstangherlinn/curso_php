2dbc35fb2c52edba23b9c9230727e600fcf48737
derikstangherlinn <derikst2@gmail.com>
2025-01-14 13:29:29 -0300

    att

--- 6_estruturas_de_controle/12_estrutura_switch/index.php
+++ 6_estruturas_de_controle/12_estrutura_switch/index.php
@@ -4,14 +4,15 @@
 
     switch ($x) {
 
-    case 0:
+    case 0: // CASO o $x for igual a 0, faça o echo do 0
         echo "x é igual a 0 <br>";
-        break;
-    case 1:
+        break; // Faz o break do case 0, somente mostrando o case 1
+    case 1: // CASO for igual a 1, faça o echo do 1
         echo "X é igual a 1 <br>";
-        break;
-      default:
+        break; // Faz o break do case 1, somente mostrando o case 0
+      default: // como se fosse um ELSE.
         echo "X não é igual a nenhum dos valores mencionados <br>";  
+
     }
 
     $y = "";
@@ -26,4 +27,5 @@
         break;
        default:
           echo "O nome não foi encontrado";
+
     }
