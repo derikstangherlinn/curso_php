c032af773b58f30d111c1db818103b8ffcbd616a
derikstangherlinn <derikst2@gmail.com>
2025-01-22 15:18:45 -0300

    att

--- Calculadora/index.php
+++ Calculadora/index.php
@@ -1,12 +1,11 @@
 <?php
 
-
 require_once 'calculadora.php';
 
 $calc = new Calculadora();
 
 
-$soma = $calc->adicao(10, 5);
+$soma = $calc->adicao(50, 5);
 $subtracao = $calc->subtracao(10, 5);
 $multiplicacao = $calc->multiplicacao(10, 5);
 $divisao = $calc->divisao(10, 5);
@@ -16,3 +15,5 @@ echo "Soma: $soma<br>";
 echo "Subtração: $subtracao<br>";
 echo "Multiplicação: $multiplicacao<br>";
 echo "Divisão: $divisao<br>";
+
+?>
