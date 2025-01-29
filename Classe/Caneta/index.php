88f7337851e747c12b3f260e45935897e95bd964
derikstangherlinn <derikst2@gmail.com>
2025-01-29 14:49:39 -0300

    att

--- Classe/Caneta/index.php
+++ Classe/Caneta/index.php
@@ -12,8 +12,8 @@
                     $c1-> cor = "Azul";
                     $c1-> ponta = 0.5;
                     $c1-> tampada = false;
-                    $c1->rabiscar();
-
+                    $c1-> tampar();
+                    $c1-> rabiscar();
                     //print_r ($c1); 
                     
 
