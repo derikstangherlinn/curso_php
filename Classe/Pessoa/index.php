499ae614dcb1ac1a536c99106369e80a504c4bfd
derikstangherlinn <derikst2@gmail.com>
2025-01-31 12:09:15 -0300

    att

--- Classe/Pessoa/index.php
+++ Classe/Pessoa/index.php
@@ -2,3 +2,14 @@
 
 require 'pessoa.php';
 
+$pessoa2 = new Pessoa();
+$pessoa2 ->nome = "Azevedo";
+$pessoa2 ->idade = 18;
+echo "<br>";
+$pessoa2->falar();
+ 
+$pessoa3 = new Pessoa();
+$pessoa3-> nome = "Stangherlin";
+$pessoa3 -> idade = 19;
+echo "<br>";
+$pessoa3->falar();
--- Classe/Pessoa/pessoa.php
+++ Classe/Pessoa/pessoa.php
@@ -3,12 +3,15 @@
 class Pessoa {
 
     public $nome;
+    public $idade;
     function falar() {
         echo "O " . $this->nome . " deu bom dia"; 
-
+        echo "<br>";
+        echo "E tem " . $this->idade . " anos"; 
  }    
 }
 
 $mensagem = new Pessoa();
 $mensagem->nome = "Derik";
+$mensagem->idade = 21;
 $mensagem->falar();  
