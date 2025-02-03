6339d01d00b08be01ec863ae954c59a6ab82ab54
derikstangherlinn <derikst2@gmail.com>
2025-02-03 14:39:21 -0300

    att

--- Classe/Caneta/index.php
+++ Classe/Caneta/index.php
@@ -12,17 +12,21 @@
                     $c1 = new Caneta;
                     $c1-> cor = "Azul";
                     $c1-> ponta = 0.5;
-                    $c1-> tampada = false;
+                    $c1-> tampada = true;
                     $c1-> tampar();
                     print_r($c1);
+                    $c1->rabiscar();
 
+                    echo "<br>";
                     echo "<br>";
 
                     $c2  = new Caneta;
                     $c2->cor = "Verde";
                     $c2->carga = 50;
-                    $c2->tampar();
-                    print_r($c2); 
+                    $c2->tampada = true;
+                    $c2->destampar();
+                    print_r($c2);
+                    $c2->rabiscar();
                     
 
             ?>
