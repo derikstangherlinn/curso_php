d411302581308eb3e71d1b8dc41fa25856c8058e
derikstangherlinn <derikst2@gmail.com>
2025-02-27 14:54:24 -0300

    att

--- 0_Classe/Lutador/index.php
+++ 0_Classe/Lutador/index.php
@@ -7,16 +7,19 @@
             <pre>
                 <?php
                         require 'lutador.php';
+                        require 'luta.php';
                         $l = array();
-                        $l[0] = new Lutador ("Derik", "França", 30, 1.75, 90.9, 11, 2, 1);
-                        $l[1] = new Lutador ("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
-                        $l[2] = new Lutador ("Snapshadow", "Eua", 36, 1.65, 80.9, 12, 2, 1);
-                        $l[3] = new Lutador ("Dead Code", "Australia", 28, 1.93, 81.6, 13, 0, 2);
-                        $l[4] = new Lutador ("UFOCobol", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
-                        $l[5] = new Lutador ("Nerdaart", "Eua", 30, 1.81, 105.7, 12, 2, 4);
-                        $l[3]->perderLuta(); 
-                        $l[3]-> apresentar();
-                        $l[3]-> status();
+                        $l[0] = new Lutador ("Derik ", "França", 30, 1.75, 68.9, 11, 2, 1);
+                        $l[1] = new Lutador ("Putscript ", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
+                        $l[2] = new Lutador ("Snapshadow ", "Eua", 36, 1.65, 80.9, 12, 2, 1);
+                        $l[3] = new Lutador ("Dead Code ", "Australia", 28, 1.93, 81.6, 13, 0, 2);
+                        $l[4] = new Lutador ("UFOCobol ", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
+                        $l[5] = new Lutador ("Nerdaart ", "Eua", 30, 1.81, 105.7, 12, 2, 4);
+                        $UEC01 = new Luta();
+                        $UEC01->marcarLuta($l[4], $l[5]);
+                        $UEC01->lutar();
+                        $l[0] ->Status();
+                        $l[1] ->Status();
                         ?>
                     </pre>
                 </body>
--- 0_Classe/Lutador/luta.php
+++ 0_Classe/Lutador/luta.php
@@ -1,5 +1,6 @@
 <?php
     require_once 'lutador.php';
+ 
     Class Luta {
         private $desafiado;
         private $desafiante;
@@ -7,10 +8,44 @@
         private $aprovada;
     
         
-        public function marcarLuta() {
+        public function marcarLuta($l1, $l2) {
+            if ($l1->getCategoria() === $l2->getCategoria() 
+                    && ($l1 != $l2)) {
+                $this->aprovada = true;
+                $this->desafiado = $l1;
+                $this->desafiante = $l2;
+            } else {
+                $this->aprovada = false;
+                $this->desafiado = null;
+                $this->desafiante = null;
+            }
         }
 
         public function lutar() {
+            if ($this->aprovada) {
+                $this->desafiado->apresentar();
+                $this->desafiante->apresentar();
+                $vencedor = rand(0, 2);
+                switch ($vencedor){
+                    case 0: 
+                        echo "<p>Empate!</p>";
+                        $this->desafiado->empatarLuta();
+                        $this->desafiante->empatarLuta();
+                        break;
+                    case 1:
+                        echo "<p>" . $this->desafiado->getNome() . "venceu </p<";
+                        $this->desafiado->ganharLuta();
+                        $this->desafiante->perderLuta();
+                        break;
+                    case 2:
+                        echo "<p>" . $this->desafiante->getNome() . "venceu </p<";
+                        $this->desafiante->ganharLuta();
+                        $this->desafiado->perderLuta();
+                        break;
+                } 
+            } else {
+                echo "<p>Luta não pode acontecer</p>";
+            }
         }
 
         function getDesafiado() {
