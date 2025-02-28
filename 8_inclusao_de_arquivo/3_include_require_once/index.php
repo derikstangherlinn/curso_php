0aace0bafe593111622fafd983fddccce2ac2ae6
derikstangherlinn <derikst2@gmail.com>
2025-02-28 12:52:25 -0300

    att

---
+++ 8_inclusao_de_arquivo/3_include_require_once/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    //Arquivos que não existem
+    //include_once "teste.php";
+
+    //Arquivos que não existem
+    include_once 'teste2.php';
+    include_once 'teste2.php';
+
+    //Arquivo que não existe
+    //require_once 'teste.php';
+
+    require_once 'teste3.php';
+    require_once 'teste3.php';
+
+?>
+
+<p>Testando código!</p>
