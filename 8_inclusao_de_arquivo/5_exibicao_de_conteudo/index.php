9c58a23a712c1ab089b347b3945be884971e5357
derikstangherlinn <derikst2@gmail.com>
2025-02-28 14:59:18 -0300

    att

---
+++ 8_inclusao_de_arquivo/5_exibicao_de_conteudo/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$nome = "Derik";
+$sobrenome = "Stangherlin";
+
+?>
+
+<form action="">
+    <div>
+        <input type="text" value="<?php $nome; ?>"> 
+    </div>
+    <div> 
+        <input type="text" value="<?php $sobrenome; ?>">
+    </div>
+    <div>
+    <input type="submit" value="enviar">
+    </div>
+</form>
