b7e61b58d58a59390806809880e45af845819904
derikstangherlinn <derikst2@gmail.com>
2025-03-05 13:38:05 -0300

    att

---
+++ 9_funcoes/4_exercicio_32/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = 5;
+$b = 10;
+$c = 20;
+
+    function multi($a, $b, $c) {
+            
+        return $a * $b * $c;
+
+    }
+
+$resultado = multi($a, $b, $c);
+echo $resultado;
---
+++ 9_funcoes/4_exercicio_32/readme.md
@@ -0,0 +1,7 @@
+Exercício 32
+
+● Crie uma função;
+
+● Define três variáveis numéricas dentro dela;
+
+● Exiba a multiplicação destes números com um echo;
