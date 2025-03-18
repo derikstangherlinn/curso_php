5bb5cb4ebe7d46e5cec74d2e2db8d30e9f31644b
derikstangherlinn <derikst2@gmail.com>
2025-03-18 13:57:29 -0300

    att

---
+++ 10_strings/2_valores_de_escape/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    header ("Content-type: text/plain");
+    
+    //pular linha
+    echo "Isso aqui vai ficar na primeira linha \n E isso na segunda linha \n";
+
+    //tab
+    echo "Testando tab \t aqui \n";
+
+    //barra invertida
+    echo "Barra invertida \\ \n";
+
+    //dolar 
+    echo "Imprimindo o dolar \$teste";
---
+++ 10_strings/2_valores_de_escape/readme.md
@@ -0,0 +1,14 @@
+Valores de escape
+
+● Podemos utilizar alguns valores que executam funções especiais em
+strings;
+
+● Precisamos utilizar aspas duplas;
+
+● Exemplos: \n = nova linha;
+
+● \t = tab;
+
+● \\ = barra invertida;
+
+● \$ = sinal de dólar;
