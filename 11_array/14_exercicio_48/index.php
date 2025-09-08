cc7640ced5c2a13caa548ee65224c285603d80bd
derikstangherlinn <derikst2@gmail.com>
2025-09-08 13:42:29 -0300

    att

---
+++ 11_array/14_exercicio_48/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    $arr = ['batata', 'maçã', 'pera', 'feijao', 'arroz'];
+
+    array_splice($arr, 2, 2);
+
+    print ($arr);
+    echo "<br>";
+
+    print_r($removidos);
---
+++ 11_array/14_exercicio_48/readme.md
@@ -0,0 +1,5 @@
+Exercício 48
+
+● Crie um array com os valores: batata, maçã, pera, feijão, arroz;
+
+● Remova pera e feijão;
