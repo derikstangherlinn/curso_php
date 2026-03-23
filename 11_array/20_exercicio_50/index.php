5b3785e6ef424f2207a1428dd1188c66a84e31ca
Derik Azevedo Stangherlin <derikst2@gmail.com>
2026-03-23 15:04:32 -0300

    att

---
+++ 11_array/20_exercicio_50/index.php
@@ -0,0 +1,23 @@
+<?php
+
+ $pessoas = [
+    'Derik' => 22,
+    'Alexia' => 25,
+    'Pedro' => 24,
+    'João' => 43,
+ ];
+
+ ?>
+
+<table>border='1'>
+    <tr>
+        <th>Nome</th>
+        <th>Idade</th>
+    </tr>
+<?php foreach ($pessoa as $nome => $idade): ?>
+    <tr>
+        <td><?= $nome; ?></td>
+        <td><?= $idade; ?></td>
+    </tr>
+ <?php endforeach; ?>
+</table>
