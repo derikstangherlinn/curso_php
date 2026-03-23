f68a5fce4518bc0e39312edddda495eb46edfc35
Derik Azevedo Stangherlin <derikst2@gmail.com>
2026-03-23 14:03:21 -0300

    att

---
+++ 11_array/19_foreach/index.php
@@ -0,0 +1,25 @@
+<?php
+
+ $Derik = [
+    'nome' => 'Derik',
+    'idade' => 22,
+    'profissao' => 'Programador'
+ ];
+
+ $alexia = [
+    'nome' => 'Alexia',
+    'idade' => 25,
+    'profissao' => 'Eng. Civil',
+ ];
+
+ foreach($matheus as $carac => $value) {
+
+    echo "$carac => $value <br>";
+
+ }
+
+ foreach($alexia as $value) {
+
+    echo "$value <br>";
+    
+ }
---
+++ 11_array/19_foreach/readme.md
@@ -0,0 +1,11 @@
+Foreach e arrays
+
+● Anteriormente vimos a estrutura foreach com arrays, podemos iterar
+facilmente com ela;
+
+● Utilizando a notação de chave => valor, temos acesso rápido também a
+arrays associativos;
+
+● Exemplo:
+
+foreach($itens as $key => $value) { }
