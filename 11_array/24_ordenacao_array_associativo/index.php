a34b316ad3664f066c9e60ef2102d668b9a954d8
Derik Azevedo Stangherlin <derikst2@gmail.com>
2026-03-27 14:17:30 -0300

    att

---
+++ 11_array/24_ordenacao_array_associativo/index.php
@@ -0,0 +1,37 @@
+<?php
+
+ $arr = [
+    'Matheus' => 29,
+    'Pedro' => 18,
+    'Joaquim' => 14,
+    'Maria' => 12,
+ ];
+
+ asort($arr);
+
+ print_r($arr)
+ echo "<br>";
+
+
+ $arr2 = [
+    'Matheus' => 12,
+    'Pedro' => 44,
+    'Joaquim' => 14,
+    'Maria' => 32,
+ ];
+
+
+ asort($arr2);
+
+ print_r($arr2);
+ echo "<br>";
+
+ ksort($arr2);
+
+ print_r($arr2);
+ echo "<br>";
+
+ krsort($arr2);
+
+ print_r($arr2);
+ echo "<br>";
---
+++ 11_array/24_ordenacao_array_associativo/readme.md
@@ -0,0 +1,6 @@
+Ordenação de arrays associativos
+
+● Para ordenar em ordem crescente pelo valor das chaves, podemos utilizar
+a função arsort;
+
+● Se quisermos ordenar o array pelas chaves, utilizamos o valor ksort;
